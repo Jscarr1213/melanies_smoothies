89ddcbde6e425b29fc1472a5e52077e109a4053a
Jscarr1213 <jesuscarreiroaguilar@gmail.com>
2026-08-11 10:17:05 -0500

    Remove unused imports
    Removed unused imports from streamlit_app.py.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
-import os
 from snowflake.snowpark.functions import col
-#from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
