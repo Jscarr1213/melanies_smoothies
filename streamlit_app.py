6a754e42fdc6e8fd337fb9ba21397a9479e4e1e7
Jscarr1213 <jesuscarreiroaguilar@gmail.com>
2026-08-11 10:02:47 -0500

    Comment out unused import statement
    Comment out the import statement for 'col' from snowflake.snowpark.functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
   """
 )
 
-from snowflake.snowpark.functions import col
+#from snowflake.snowpark.functions import col
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
